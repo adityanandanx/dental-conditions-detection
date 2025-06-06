ec69f45d95afbda44dcb55f7051825f575bb91e8
adityanandanx <nandanaditya985@gmail.com>
2025-06-07 01:59:37 +0530

    feat: basic ui

---
+++ frontend/src/components/nav.tsx
@@ -0,0 +1,21 @@
+import { GlassesIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const Nav = () => {
+  return (
+    <header>
+      <nav className="flex items-center justify-between px-10 py-5 border-b">
+        <div className="flex gap-3 items-center">
+          <GlassesIcon className="text-primary" size={32} />
+          <h1 className="font-bold leading-none">
+            Dental X-Ray <br />{" "}
+            <span className="text-muted-foreground">Analysis AI</span>
+          </h1>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
