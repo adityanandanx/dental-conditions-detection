bff299c8cabfd01086dc7dfe1717f11412fe07b1
adityanandanx <nandanaditya985@gmail.com>
2025-06-07 02:33:25 +0530

    feat: basic dropzone

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { DCMDropzone } from "@/components/dcm-dropzone";
 import Nav from "@/components/nav";
 import { Button } from "@/components/ui/button";
 
@@ -8,6 +10,7 @@ export default function Home() {
       <main className="flex flex-1">
         <div className="w-lg p-10">
           <h1 className="text-2xl font-bold mb-5">X-Ray Image</h1>
+          <DCMDropzone />
           <div className="border border-dashed border-muted-foreground p-10 flex flex-col items-center justify-center gap-2 h-96 text-center rounded-4xl">
             <h1 className="text-lg font-bold">Upload DICOM Image</h1>
             <p>
---
+++ frontend/src/components/dcm-dropzone.tsx
@@ -0,0 +1,93 @@
+"use client";
+import {
+  Dropzone,
+  DropZoneArea,
+  DropzoneDescription,
+  DropzoneFileList,
+  DropzoneFileListItem,
+  DropzoneMessage,
+  DropzoneRemoveFile,
+  DropzoneTrigger,
+  useDropzone,
+} from "@/components/ui/dropzone";
+import { CloudUploadIcon, Trash2Icon } from "lucide-react";
+
+export function DCMDropzone() {
+  const dropzone = useDropzone({
+    onDropFile: async (file: File) => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return {
+        status: "success",
+        result: URL.createObjectURL(file),
+      };
+    },
+    validation: {
+      accept: {
+        "application/dicom": [".dcm", ".rvg"],
+      },
+      maxSize: 10 * 1024 * 1024, // 10 MB
+    },
+  });
+
+  return (
+    <div className="not-prose flex flex-col gap-4">
+      <Dropzone {...dropzone}>
+        <div className="">
+          <div className="flex flex-col mb-3">
+            <DropzoneDescription>
+              Please select DICOM (.dcm/.rvg) files you want to analyse
+            </DropzoneDescription>
+            <DropzoneMessage />
+          </div>
+          <DropZoneArea className=" border-dashed border-muted-foreground rounded-md">
+            <DropzoneTrigger className="flex flex-col items-center gap-4 bg-transparent w-full h-full text-center text-sm">
+              <CloudUploadIcon className="size-8" />
+              <div>
+                <p className="font-semibold">Upload DICOM files</p>
+                <p className="text-sm text-muted-foreground">
+                  Click here or drag and drop to upload
+                </p>
+              </div>
+            </DropzoneTrigger>
+          </DropZoneArea>
+        </div>
+
+        <DropzoneFileList className="gap-3 p-0">
+          {dropzone.fileStatuses.map((file) => (
+            <DropzoneFileListItem
+              className="overflow-hidden rounded-md bg-secondary p-0 shadow-sm"
+              key={file.id}
+              file={file}
+            >
+              {file.status === "pending" && (
+                <div className="aspect-video animate-pulse bg-black/20" />
+              )}
+              {file.status === "success" && (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  src={file.result}
+                  alt={`uploaded-${file.fileName}`}
+                  className="aspect-video object-cover"
+                />
+              )}
+              <div className="flex items-center justify-between p-2 pl-4">
+                <div className="min-w-0">
+                  <p className="truncate text-sm">{file.fileName}</p>
+                  <p className="text-xs text-muted-foreground">
+                    {(file.file.size / (1024 * 1024)).toFixed(2)} MB
+                  </p>
+                </div>
+                <DropzoneRemoveFile
+                  variant="ghost"
+                  className="shrink-0 hover:outline"
+                >
+                  <Trash2Icon className="size-4" />
+                </DropzoneRemoveFile>
+              </div>
+            </DropzoneFileListItem>
+          ))}
+        </DropzoneFileList>
+      </Dropzone>
+    </div>
+  );
+}
